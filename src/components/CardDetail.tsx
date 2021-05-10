a2be10a23dc30c5793ec751f4bacc7d44f447a2e
viniciusBotelho625 <viniciusoficialbotelho@outlook.com>
2021-05-10 16:00:58 +0000

    Estilização da tela "Detalhe do Pedido"

--- src/components/CardDetail.tsx
+++ src/components/CardDetail.tsx
@@ -7,7 +7,7 @@ import pizzaImg from '../assets/pizza.jpg';
 
 export function CardDetail() {
     return(
-        <div className={styles.CardDetail}>
+        <div className={styles.cardDetail}>
             <div className={styles.cardDetailImage}>
                 <button type="button">
                     <span>
@@ -18,35 +18,39 @@ export function CardDetail() {
                     <img src={pizzaImg} alt="Imagem do pedido"/>
                 </figure>
             </div>
-            <div className={styles.CardDetailInformation}>
-                <p className={styles.title}>Pizza de Peperoni</p>
-                <span>
-                    <FontAwesomeIcon icon={faStar}/>
-                </span>
-                <span>
-                    <FontAwesomeIcon icon={faStar}/>
-                </span>
-                <span>
-                    <FontAwesomeIcon icon={faStar}/>
-                </span>
-                <span>
-                    <FontAwesomeIcon icon={faStar}/>
-                </span>
-                <span>
-                    <FontAwesomeIcon icon={faStar}/>
-                </span> 
+            <div className={styles.cardDetailInformation}>
+                <div className={styles.cardDetailAvaliation}>
+                    <p className={styles.title}>Pizza de Peperoni</p>
+                    <span>
+                        <FontAwesomeIcon icon={faStar}/>
+                    </span>
+                    <span>
+                        <FontAwesomeIcon icon={faStar}/>
+                    </span>
+                    <span>
+                        <FontAwesomeIcon icon={faStar}/>
+                    </span>
+                    <span>
+                        <FontAwesomeIcon icon={faStar}/>
+                    </span>
+                    <span>
+                        <FontAwesomeIcon icon={faStar}/>
+                    </span> 
+                </div>
                 <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Fusce faucibus commodo orci at aliquet. Nulla nec nisi rhoncus.</p>  
-                <div className={styles.orderSize}>
+                <div className={styles.order}>
                     <p>Escolha o tamanho</p>
-                    <button type="button">P</button>
-                    <button type="button">M</button>
-                    <button type="button">G</button>
+                    <div className={styles.orderOptionsSize}>
+                        <button type="button">P</button>
+                        <button type="button">M</button>
+                        <button type="button">G</button>
+                    </div>
                 </div>
                 <div className={styles.orderPrice}>
                     <p>R$ 29,99</p>
                 </div>
-                <button type="submit">Comprar</button>
+                <button type="submit" className={styles.buttonPay}>Comprar</button>
             </div>
         </div>
     );
