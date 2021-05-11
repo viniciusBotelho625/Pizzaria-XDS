06b9d7b63aacfc3b4bbef19ed2214d37f210a6d5
viniciusBotelho625 <viniciusoficialbotelho@outlook.com>
2021-05-11 20:28:50 +0000

    Conectando com a API do cardapio

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 import  {SideBar}  from '../components/SideBar';
-
 import { InputSearch } from '../components/InputSearch';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import pizzaImg from '../assets/pizza.jpg';
 
+import styles from '../styles/pages/List.module.css';
 import api from '../services/api';
 
-import styles from '../styles/pages/List.module.css';
 import { useHistory } from 'react-router';
 
 export default function List() {
@@ -19,12 +17,15 @@ export default function List() {
 
     // const history = useHistory(); 
 
+    console.log({
+        pizzas
+    })
+
     useEffect(() => {
         api.get('https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza', {
 
         }).then(response => {
             setPizzas(response.data)
-            console.log(response)
         })
     })
     
@@ -35,35 +36,37 @@ export default function List() {
         <main>
             <p className={styles.title}>Escolha seu Sabor</p>
             <section>
-                <div className={styles.cardMenu}>
-                    <div className={styles.cardHeader}>
-                        <figure>
-                            <img src={pizzaImg} alt="Imagem ilustrativa da Pizza"/>
-                        </figure>
+                {pizzas.map(pizza => (
+                    <div className={styles.cardMenu}>
+                        <div className={styles.cardHeader}>
+                            <figure>
+                                <img src={pizza.imageUrl} alt="Imagem ilustrativa da Pizza"/>
+                            </figure>
+                        </div>
+                        <div className={styles.cardBody}>
+                            <p>{pizza.name}</p>
+                            <span>
+                                <FontAwesomeIcon icon={faStar}/>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faStar}/>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faStar}/>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faStar}/>
+                            </span>
+                            <span>
+                                <FontAwesomeIcon icon={faStar}/>
+                            </span>       
+                        </div>
+                        <div className={styles.cardFooter}>
+                            <p>a partir de:</p>
+                            <p>{pizza.priceP}</p>
+                        </div>
                     </div>
-                    <div className={styles.cardBody}>
-                        <p></p>
-                        <span>
-                            <FontAwesomeIcon icon={faStar}/>
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faStar}/>
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faStar}/>
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faStar}/>
-                        </span>
-                        <span>
-                            <FontAwesomeIcon icon={faStar}/>
-                        </span>       
-                    </div>
-                    <div className={styles.cardFooter}>
-                        <p>a partir de:</p>
-                        <p>R$ 29,00</p>
-                    </div>
-                </div>
+                ))}
             </section>
         </main>
         </>
